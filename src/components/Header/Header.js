6cb52f81c7d85545f76a6b0e540ec65004f7ab17
motlubor <motlubor2591997@gmail.com>
2022-03-26 10:36:21 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-success'>Laptop Zone</h1>
+            <h3 className='text-info'>Choose 4 Laptops</h3>
+        </div>
+    );
+};
+
+export default Header;
